c8d7fc47f197b39412f9478ea800410fd8dc645b
Alpharex121 <arpitkoshta08@gmail.com>
2024-07-21 18:32:47 +0530

    removing .ts

--- src/app/api/accept-messages/route.ts
+++ src/app/api/accept-messages/route.ts
@@ -1,5 +1,5 @@
 import { getServerSession } from 'next-auth/next';
-import { authOptions } from '../auth/[...nextauth]/[...nextauth].ts';
+import { authOptions } from '../auth/[...nextauth]/[...nextauth]';
 import dbConnect from '@/lib/dbConnect';
 import UserModel from '@/model/User';
 import { User } from 'next-auth';
