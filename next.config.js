39c93ff78d2e3268d7cdbc754a2d4382e68af4f7
Anmol <anmolawasthi117@gmail.com>
2024-07-22 21:49:32 +0530

    openai api key updated

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
           { key: "Access-Control-Allow-Credentials", value: "true" },
           {
             key: "Access-Control-Allow-Origin",
-            value: "https://whisper-bay-ten.vercel.app",
+            value: "https://whisper-bay-ten.verce",
           }, // replace this your actual origin
           {
             key: "Access-Control-Allow-Methods",
